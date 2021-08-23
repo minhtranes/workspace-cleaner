f6c7f98f22132e673c05ca2c32fa5e7d7737d1c2
Minh Tran <wisekingtran@gmail.com>
2021-08-23 22:25:44 +0700

    Initialize project

---
+++ src/main/java/vn/minhtran/study/wc/WorkspaceCleanerApplication.java
@@ -0,0 +1,13 @@
+package vn.minhtran.study.wc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceCleanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspaceCleanerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/minhtran/study/wc/WorkspaceCleanerApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.minhtran.study.wc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspaceCleanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
